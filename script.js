c15f7898ca9b98c891982c81defe692068ae28b5
Frexet <frexet@gmail.com>
2025-04-10 10:30:55 +0200

    Updatering av sida

--- script.js
+++ script.js
@@ -16,22 +16,3 @@ function toggleSection(id) {
 document.addEventListener("DOMContentLoaded", () => {
     showSection('home');
 });
-
-// Function to load SoundCloud mix based on selection
-function loadMix(mix) {
-    const playerDiv = document.getElementById("soundcloud-player");
-    let embedUrl = "";
-
-    if (mix === "tech-house") {
-        embedUrl = "https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/rexet/tech-house-mix&color=%2300adb5";
-    } else if (mix === "edm-remix") {
-        embedUrl = "https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/rexet/first-remix-edm&color=%2300adb5";
-    }
-
-    if (embedUrl) {
-        playerDiv.innerHTML = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="${embedUrl}"></iframe>`;
-        console.log(`Loaded SoundCloud mix: ${embedUrl}`);
-    } else {
-        console.error("No valid mix selected.");
-    }
-}
